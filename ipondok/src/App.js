7a9bc275d2c448898e3c46a3bcd76505f1d46fd8
Benylaode <bayubulan659@gmail.com>
2025-03-06 23:02:32 +0800

    menjadikan yang jadi

--- ipondok/src/App.js
+++ ipondok/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import {Home, PondokDetail, Pemesanan} from "./pages/"; // Halaman utama
 import { PondokProvider } from './PondokContext';
 import './App.css'; // Ganti dengan nama file CSS Anda
